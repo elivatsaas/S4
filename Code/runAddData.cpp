112f69f2126cbacf5f62b1be87781916620d58f6
elivatsaas <157637354+elivatsaas@users.noreply.github.com>
2024-02-26 14:36:49 -0700

    Update runAddData.cpp

--- Code/runAddData.cpp
+++ Code/runAddData.cpp
@@ -1,5 +1,6 @@
 #include "addData.h"
 #include "sqlConnection.h"
+
 using namespace std;
 
 
@@ -7,7 +8,6 @@ int main(int argc, char **argv)
 {
     dbo::Session session;
 	S4::InitializeSession(session);
-    dbo::Transaction transaction(session);
 
     string dataType;
     cout << "Choose a Data Type to Insert:\nEmployee\nShift\nRole\nStore\nAvailabiity\nSchedule\nDesiredShiftHours\nEmployeeRole\nEmployeeStore\n";
