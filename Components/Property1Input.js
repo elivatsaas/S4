bf428af67ab72a14f06bf899d8a33beb5c785faa
elivatsaas <157637354+elivatsaas@users.noreply.github.com>
2024-04-23 10:37:45 -0700

    Delete Components directory

--- Components/Property1Input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Property1Input.css'
-export default function Property1Input (props) {
-	return (
-		<div className={`Property1Input_Property1Input ${props.className}`}>
-			<span className='Text'> </span>
-		</div>
-	)
-}
